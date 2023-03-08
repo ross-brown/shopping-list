f37333d98f1e06c295c1b276abd3a4b701cbfe6a
ross-brown <rbrown_98@yahoo.com>
2023-03-07 23:18:01 -0600

    prevent duplicate items from being added to the list

--- script.js
+++ script.js
@@ -31,6 +31,11 @@ function onAddItemSubmit(e) {
     itemToEdit.classList.remove("edit-mode");
     itemToEdit.remove();
     isEditMode = false;
+  } else {
+    if (checkIfItemExists(newItem)) {
+      alert("That item already exists");
+      return;
+    }
   }
 
   //Create item DOM element
@@ -98,6 +103,12 @@ function onClickItem(e) {
   } else if (e.target.tagName === "LI") {
     setItemToEdit(e.target);
   }
+}
+
+function checkIfItemExists(item) {
+  const itemsFromStorage = getItemsFromStorage();
+  const lowerCaseStorage = itemsFromStorage.map(i => i.toLowerCase());
+  return lowerCaseStorage.includes(item.toLowerCase())
 }
 
 function setItemToEdit(item) {
